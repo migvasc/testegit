e296b619ae47ee43a6dc524ea81833fe35e2f2b3
migvasc <miguel.vasconcelos@usp.br>
2016-10-17 22:40:55 +0000

    TESTE PHP CONEXAO POSTGRESQL 8

--- index.php
+++ index.php
@@ -7,22 +7,4 @@
 echo "Ola";
 die();
 
-function pg_connection_string() { 
-	return "dbname= d2b8h06bak4ivo host= ec2-23-23-176-135.compute-1.amazonaws.com user=kykiyayfiroilv password=rYI3CtNdygxh8_gn358BjPutkw port=5432 sslmode=require"
-} 
-
-
-$conn = pg_connect(pg_connection_string()); 
-$result= pg_query($conn, "SELECT * FROM x WHERE a=b;");
-if  (!$result) {
-   echo "query did not execute";
-}
-if (pg_num_rows($result) == 0) {
-   echo "0 records"
- }
- else {
-   while ($row = pg_fetch_array($result)) {
-	echo "one row"
-   }
- }
  ?>
