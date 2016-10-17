1929bcc51032226705d37146839a834c9daf0d0f
migvasc <miguel.vasconcelos@usp.br>
2016-10-17 02:35:18 +0000

    sera q vai?

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 
-echo 'OLARSSS CARALHEOW!';
+echo 'OLARSSS CARALHEOW! O PAPAI CHEGOU AQUI';
 
 ?>
 </body>
