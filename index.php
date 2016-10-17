98f980b1ccf1dfe6fa4548ea8a2faf61f6f68946
migvasc <miguel.vasconcelos@usp.br>
2016-10-17 22:29:59 +0000

    TESTE PHP CONEXAO POSTGRESQL 5

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ function pg_connection_string() {
 	return "dbname= d2b8h06bak4ivo host= ec2-23-23-176-135.compute-1.amazonaws.com user=kykiyayfiroilv password=rYI3CtNdygxh8_gn358BjPutkw port=5432 sslmode=require"
 } 
 
+echo "Ola";
+die();
 
 $conn = pg_connect(pg_connection_string()); 
 $result= pg_query($conn, "SELECT * FROM x WHERE a=b;");
