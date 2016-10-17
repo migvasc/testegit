4ae1286d26800258f029eb3c9423a7d9478239ea
migvasc <miguel.vasconcelos@usp.br>
2016-10-17 22:35:16 +0000

    TESTE CONEXAO

--- index.php
+++ index.php
@@ -1,24 +1,11 @@
+
+
+
 <?php
+// A simple web site in Cloud9 that runs through Apache
+// Press the 'Run' button on the top to start the web server,
+// then click the URL that is emitted to the Output tab of the console
 
+echo 'TESTE PARA CONFIRMAR SE ESTA FUNCIONADO 4';
 
-echo "Ola";
-die();
-
-function pg_connection_string() { 
-	return "dbname= d2b8h06bak4ivo host= ec2-23-23-176-135.compute-1.amazonaws.com user=kykiyayfiroilv password=rYI3CtNdygxh8_gn358BjPutkw port=5432 sslmode=require"
-} 
-
-
-$conn = pg_connect(pg_connection_string()); 
-$result= pg_query($conn, "SELECT * FROM x WHERE a=b;");
-if  (!$result) {
-   echo "query did not execute";
-}
-if (pg_num_rows($result) == 0) {
-   echo "0 records"
- }
- else {
-   while ($row = pg_fetch_array($result)) {
-	echo "one row"
-   }
- }
+?>
