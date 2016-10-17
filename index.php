8c20b37ae64071d49a6dbffc0b11ee2c1c17706d
migvasc <miguel.vasconcelos@usp.br>
2016-10-17 22:42:49 +0000

    TESTE PHP CONEXAO POSTGRESQL 9

--- index.php
+++ index.php
@@ -7,4 +7,11 @@
 echo "Ola";
 die();
 
+echo "Ola2";
+
+function pg_connection_string() { 
+	return "dbname= d2b8h06bak4ivo host= ec2-23-23-176-135.compute-1.amazonaws.com user=kykiyayfiroilv password=rYI3CtNdygxh8_gn358BjPutkw port=5432 sslmode=require"
+} 
+
+echo "Ola3";
  ?>
