d5660a93360e6fbe7a29888864d6474e0d65667e
migvasc <miguel.vasconcelos@usp.br>
2016-10-18 14:59:34 +0000

    TESTE CONEXAO PHP POSTGRESQL HEROKU

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <?php
 
 $conn = pg_connect("dbname=d2b8h06bak4ivo host=ec2-23-23-176-135.compute-1.amazonaws.com user=kykiyayfiroilv password=rYI3CtNdygxh8_gn358BjPutkw port=5432 sslmode=require"); 
-$result= pg_query($conn, "SELECT * FROM usuario;");
+$result= pg_query($conn, "SELECT * FROM usuario");
 if  (!$result) {
    echo "query did not execute";
 }
