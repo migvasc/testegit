18e11d6da610dfe6de77f59607ff52842cc5605e
migvasc <miguel.vasconcelos@usp.br>
2016-10-18 17:18:54 +0000

    TESTE CONEXAO PHP POSTGRESQL HEROKU 7

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 # This function reads your DATABASE_URL config var and returns a connection
 # string suitable for pg_connect. Put this in your app.
 function pg_connection_string_from_database_url() {
-extract(parse_url($_ENV["postgres://kykiyayfiroilv:rYI3CtNdygxh8_gn358BjPutkw@ec2-23-23-176-135.compute-1.amazonaws.com:5432/d2b8h06bak4ivo"]));
+extract(parse_url($_ENV["DATABASE_URL"]));
 print "user=$user\n";
 print "password=$pass\n";
 return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
