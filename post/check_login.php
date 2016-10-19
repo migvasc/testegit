c9d4195af7083bb7a2ffaa271928b108176c4a72
migvasc <miguel.vasconcelos@usp.br>
2016-10-19 18:21:48 +0000

    tentando arrumar login 1

--- post/check_login.php
+++ post/check_login.php
@@ -1,19 +1,10 @@
 <?php
 
 require("../conn.php");
-$result = pg_query($conn, "select email from usuario where email = " . $_POST['email'] ." and senha = " . $_POST['senha']);
+$result = pg_query($conn, "select email from usuario where email = " . htmlspecialchars($_POST['email']) ." and senha = " . htmlspecialchars($_POST['senha']));
 
 if(pg_num_rows($result))
 	return true;
 else
 	return false;
-// print "<pre>\n";
-// if (!pg_num_rows($result)) {
-// print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
-// } else {
-// print "Tables in your database:\n";
-// while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
-// }
-// print "\n";
-   
 ?>
