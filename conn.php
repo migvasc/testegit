edbcf2450fc1182034bcf354dc9ae14404d30bb4
migvasc <miguel.vasconcelos@usp.br>
2016-10-20 17:47:28 +0000

    verificando a query de consulta de login 5 - arrumando a conn.php

--- conn.php
+++ conn.php
@@ -1,5 +1,7 @@
 <?php
 
+extract(parse_url($_ENV["DATABASE_URL"]));
+
 $url = "user=$user password=$pass host=$host sslmode=require dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
 
 $conn = pg_connect($url);
