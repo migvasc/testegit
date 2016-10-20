0cb888b6c437681e632d2f0b1aab00361cfabcb3
migvasc <miguel.vasconcelos@usp.br>
2016-10-20 20:39:14 +0000

    Revert "Testando a query de consulta do login novamente 3"
    This reverts commit 6d739d5943e0c66d28aeca41fd0d842995ad85c4.

--- assets/post/check_login.php
+++ assets/post/check_login.php
@@ -4,7 +4,7 @@ require("../../conn.php");
 $result = pg_query($conn, "select email from usuario where email = " . htmlspecialchars($_POST['email']) ." and senha = " . htmlspecialchars($_POST['senha']));
 
 if(pg_num_rows($result))
-	echo ("oi");
+	echo (pg_num_rows($result));
 
 //if(htmlspecialchars($_POST['email']) == "teste@teste.com" && htmlspecialchars($_POST['senha']) == "admin123")
 //	echo "achou!";
