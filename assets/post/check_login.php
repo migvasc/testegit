4c08df9e4fd00c82649d9bcf79b56ff1b6df6d13
migvasc <miguel.vasconcelos@usp.br>
2016-10-20 20:39:21 +0000

    Revert "Arrumando as informações pós login 6"
    This reverts commit d22edd4dfc24020f9bcebb6309437fcbf841a682.

--- assets/post/check_login.php
+++ assets/post/check_login.php
@@ -1,13 +1,14 @@
 <?php
 
-//require("../../conn.php");
-//$result = pg_query($conn, "select email from usuario where email = '" . htmlspecialchars($_POST['email']) ."' and senha = '" . htmlspecialchars($_POST['senha'])). "'";
+require("../../conn.php");
+$result = pg_query($conn, "select email from usuario where email = '" . htmlspecialchars($_POST['email']) ."' and senha = '" . htmlspecialchars($_POST['senha'])). "'";
 
-//if(pg_num_rows($result))
-//	echo (pg_num_rows($result));
-
-if(htmlspecialchars($_POST['email']) == "teste@teste.com" && htmlspecialchars($_POST['senha']) == "admin123")
-	echo "achou!";
+if(pg_num_rows($result)){
+	//$row = pg_fetch_row($result);
+	echo ("ola");
+}
+//if(htmlspecialchars($_POST['email']) == "teste@teste.com" && htmlspecialchars($_POST['senha']) == "admin123")
+//	echo "achou!";
 else
 	echo null;
 ?>
