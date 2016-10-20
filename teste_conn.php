c15ab3e65fb9216e146a8d0ddeeb9a6b6a970cc7
migvasc <miguel.vasconcelos@usp.br>
2016-10-20 17:35:28 +0000

    verificando a query de consulta de login 2

--- teste_conn.php
+++ teste_conn.php
@@ -1,13 +1,13 @@
 <?php
 
 require("conn.php");
-//$result = pg_query($conn, "select * from usuario");
+$result = pg_query($conn, "select * from usuario");
 
-$result = pg_query($conn, "select email from usuario where email = 'teste@teste.com' and senha = 'admin123'");
+//$result = pg_query($conn, "select email from usuario where email = 'teste@teste.com' and senha = 'admin123'");
 
 
 if (!pg_num_rows($result)) {
-	print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
+	print("Your connection is working, but your database is empty");
 } else {
 	print "Data in your database:\n";
 	while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
