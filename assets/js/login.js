537ce1b2f7a7a793c586eafb7c9244ac82e8099c
migvasc <miguel.vasconcelos@usp.br>
2016-10-20 18:21:36 +0000

    Arrumando as informações pós login 7

--- assets/js/login.js
+++ assets/js/login.js
@@ -29,13 +29,13 @@ $(function() {
                         $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
                         $('#success > .alert-success')
-                            .append("<strong>Your message has been sent. </strong>");
+                            .append("<strong>Usuário encontrado! </strong>");
                         $('#success > .alert-success')
                             .append('</div>');
     
                         //clear all fields
                         alert("Seja bem-vindo(a), usuario "+resposta);
-                        $('#contactForm').trigger("reset");
+                        $('#loginForm').trigger("reset");
                     }
                     else{
                         $('#success').html("<div class='alert alert-danger'>");
