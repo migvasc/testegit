2d29c92055cb409c90686b39225e7365ca4d0f05
migvasc <miguel.vasconcelos@usp.br>
2016-10-20 17:28:26 +0000

    verificando a query de consulta de login

--- teste_conn.php
+++ teste_conn.php
@@ -1,7 +1,10 @@
 <?php
 
 require("conn.php");
-$result = pg_query($conn, "select * from usuario");
+//$result = pg_query($conn, "select * from usuario");
+
+$result = pg_query($conn, "select email from usuario where email = 'teste@teste.com' and senha = 'admin123'");
+
 
 if (!pg_num_rows($result)) {
 	print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
