1484ff0641b93c1e67852356f4b6dfd26e98dd9f
migvasc <miguel.vasconcelos@usp.br>
2016-10-20 21:06:46 +0000

    ainda tentando fazer o login funcionar - verificando se o problema é de fato carateres especiais com a substituicao dos parametros pela string

--- assets/post/check_login.php
+++ assets/post/check_login.php
@@ -1,7 +1,7 @@
 <?php
 
 require("../../conn.php");
-$result = pg_query($conn, "select email from usuario where email = '" . pg_escape_string($_POST['email'].split("@")[0]) ."' and senha = '" . pg_escape_string($_POST['senha'].split("m")[1])). "'";
+$result = pg_query($conn, "select email from usuario where email = 'teste@teste.com' and senha = 'admin123'");
  
 if(pg_num_rows($result))
 	echo ("achou!");
