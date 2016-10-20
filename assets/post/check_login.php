eb25d2c5cb0856c0dfbf57a7aa3e50387bb8bc9a
migvasc <miguel.vasconcelos@usp.br>
2016-10-20 20:42:34 +0000

    verificando se a conexao com o banco esta finalmente correta para podermos fazer o login

--- assets/post/check_login.php
+++ assets/post/check_login.php
@@ -1,13 +1,13 @@
 <?php
 
-//require("../../conn.php");
-//$result = pg_query($conn, "select email from usuario where email = '" . htmlspecialchars($_POST['email']) ."' and senha = '" . htmlspecialchars($_POST['senha'])). "'";
+require("../../conn.php");
+$result = pg_query($conn, "select email from usuario where email = '" . htmlspecialchars($_POST['email']) ."' and senha = '" . htmlspecialchars($_POST['senha'])). "'";
+ 
+if(pg_num_rows($result))
+	echo (pg_num_rows($result));
 
-//if(pg_num_rows($result))
-//	echo (pg_num_rows($result));
-
-if(htmlspecialchars($_POST['email']) == "teste@teste.com" && htmlspecialchars($_POST['senha']) == "admin123")
-	echo "achou!";
+//if(htmlspecialchars($_POST['email']) == "teste@teste.com" && htmlspecialchars($_POST['senha']) == "admin123")
+//	echo "achou!";
 else
 	echo null;
 ?>
