92cacd08f14173226ca18adb67c6ec76ffb366ad
migvasc <miguel.vasconcelos@usp.br>
2016-10-20 21:39:48 +0000

    arrumando mensagem quando realiza login

--- assets/post/check_login.php
+++ assets/post/check_login.php
@@ -4,7 +4,7 @@ require("../../conn.php");
 $result = pg_query($conn, "select email from usuario where email = '". htmlentities($_POST['email'], ENT_QUOTES, "UTF-8")."' and senha = '" . htmlentities($_POST['senha'], ENT_QUOTES, "UTF-8") ."'");
  
 if(pg_num_rows($result))
-	echo ("email");
+	echo (pg_fetch_row($result)[0]);
 
 //if(htmlspecialchars($_POST['email']) == "teste@teste.com" && htmlspecialchars($_POST['senha']) == "admin123")
 //	echo "achou!";
