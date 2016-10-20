9feb3cae8e650405cabc78cd587854bf2f4178a2
migvasc <miguel.vasconcelos@usp.br>
2016-10-20 20:39:22 +0000

    Revert "Arrumando as informações pós login 5"
    This reverts commit ca98581e447412e8de19dd63dce3afa4adb67ad2.

--- assets/post/check_login.php
+++ assets/post/check_login.php
@@ -4,9 +4,11 @@ require("../../conn.php");
 $result = pg_query($conn, "select email from usuario where email = '" . htmlspecialchars($_POST['email']) ."' and senha = '" . htmlspecialchars($_POST['senha'])). "'";
 
 if(pg_num_rows($result)){
-	//$row = pg_fetch_row($result);
-	echo ("ola");
+	$row = pg_fetch_row($result);
+	echo ($row[0]);
 }
+	
+
 //if(htmlspecialchars($_POST['email']) == "teste@teste.com" && htmlspecialchars($_POST['senha']) == "admin123")
 //	echo "achou!";
 else
