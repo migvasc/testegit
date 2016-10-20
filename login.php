0a80d40fb9bdf3bb4d4ed3ff1dd8861eee3150db
migvasc <miguel.vasconcelos@usp.br>
2016-10-20 21:12:26 +0000

    ainda tentando fazer o login funcionar - verificando se o problema é de fato carateres especiais com a inclusão um comando no htlm e a utilização de outra função no check_login.php

--- login.php
+++ login.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
+    <meta http-equiv = "Content-Type" content = "text/html; charset=utf-8" />
 
     <title>Pet Ajuda | Login</title>
 
