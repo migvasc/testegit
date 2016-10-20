e1e3218471f9ba7066761e5dc31022cb75196e89
migvasc <miguel.vasconcelos@usp.br>
2016-10-20 16:45:03 +0000

    adicionando a impressao da resposta do ajax para verificar se é o esperado 5

--- assets/post/check_login.php
+++ assets/post/check_login.php
@@ -3,9 +3,11 @@
 require("../../conn.php");
 $result = pg_query($conn, "select email from usuario where email = '" . htmlspecialchars($_POST['email']) ."' and senha = '" . htmlspecialchars($_POST['senha'])). "'";
 
-if(pg_num_rows($result))
-	echo (pg_num_rows($result));
+vardump($result);
 
-else
-	echo null;
+//if(pg_num_rows($result))
+//	echo (pg_num_rows($result));
+
+//else
+//	echo null;
 ?>
