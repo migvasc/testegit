167ea6a68b479612d1df5be936dad1fca4eda059
migvasc <miguel.vasconcelos@usp.br>
2016-10-20 11:10:16 +0000

    tentando achar o erro 500 retornado pelo chech_login

--- assets/post/check_login.php
+++ assets/post/check_login.php
@@ -4,7 +4,7 @@ require("../conn.php");
 $result = pg_query($conn, "select email from usuario where email = '" . htmlspecialchars($_POST['email']) ."' and senha = '" . htmlspecialchars($_POST['senha'])). "'";
 
 if(pg_num_rows($result))
-	return true;
+	return pg_num_rows($result);
 else
 	return false;
 ?>
