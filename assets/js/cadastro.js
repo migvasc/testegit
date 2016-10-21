0e9a9a95fd52bae685aab62bca9b3dfccfb9cf65
migvasc <miguel.vasconcelos@usp.br>
2016-10-21 02:05:05 +0000

    implementando cadastro no bd 8 - adicionando checagem por senhas iguais

--- assets/js/cadastro.js
+++ assets/js/cadastro.js
@@ -16,47 +16,55 @@ $(function() {
             var phone = $("input#phone").val();
             var senha1 = $("input#senha1").val();
             var senha2 = $("input#senha2").val();
-
-
-            $.ajax({
-                url: "assets/post/check_cadastro.php",
-                type: "POST",
-                data: {
-                    nome: nome,
-                    sobrenome: sobrenome,
-                    email: email,
-                    phone: phone,
-                    senha1: senha1
-                },
-                cache: false,
-                success: function(resposta) {
-                    if(resposta != null && resposta != ""){
-                        alert("Bem-vindo(a), "+resposta);
-                        $('#success').html("<div class='alert alert-success'>");
-                        $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            
+            if(senha1 != senha2){
+                $('#success').html("<div class='alert alert-danger'>");
+                $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                $('#success > .alert-danger').append("<strong> As senhas não conferem!");
+                $('#success > .alert-danger').append('</div>');
+            }    
+            else{
+                $.ajax({
+                    url: "assets/post/check_cadastro.php",
+                    type: "POST",
+                    data: {
+                        nome: nome,
+                        sobrenome: sobrenome,
+                        email: email,
+                        phone: phone,
+                        senha1: senha1
+                    },
+                    cache: false,
+                    success: function(resposta) {
+                        if(resposta != null && resposta != ""){
+                            alert("Bem-vindo(a), "+resposta);
+                            $('#success').html("<div class='alert alert-success'>");
+                            $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                                .append("</button>");
+                            $('#success > .alert-success')
+                                .append("<strong>Usuário cadastrado com sucesso. </strong>");
+                            $('#success > .alert-success')
+                                .append('</div>');
+                        }
+                        else{
+                            $('#success').html("<div class='alert alert-danger'>");
+                            $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
-                        $('#success > .alert-success')
-                            .append("<strong>Usuário cadastrado com sucesso. </strong>");
-                        $('#success > .alert-success')
-                            .append('</div>');
-                    }
-                    else{
+                            $('#success > .alert-danger').append("<strong>E-mail já cadastrado!");
+                            $('#success > .alert-danger').append('</div>');
+                        }
+                    },
+                    error: function() {
+                        // Fail message
                         $('#success').html("<div class='alert alert-danger'>");
                         $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                        $('#success > .alert-danger').append("<strong>E-mail já cadastrado!");
+                            .append("</button>");
+                        $('#success > .alert-danger').append("<strong> Não conseguimos acessar nosso servidor :/ Desculpe-nos o transtorno e tente novamene mais tarde!");
                         $('#success > .alert-danger').append('</div>');
-                    }
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong> Não conseguimos acessar nosso servidor :/ Desculpe-nos o transtorno e tente novamene mais tarde!");
-                    $('#success > .alert-danger').append('</div>');
-                },
-            });
+                    },
+                });
+            }
         },
         filter: function() {
             return $(this).is(":visible");
