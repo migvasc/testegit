2ec0065f050ecdefa3cbf9ddee5178a87a54f76d
migvasc <miguel.vasconcelos@usp.br>
2016-10-21 01:24:49 +0000

    implementando cadastro no bd 2- faltou chamar os js --

--- cadastro.php
+++ cadastro.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Pet Ajuda | Login</title>
+    <title>Pet Ajuda | Cadastro</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
@@ -124,6 +124,22 @@
                 </div>
             </div>
         </div>
+        
+     <!-- jQuery -->
+    <script src="assets/js/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="assets/js/jqBootstrapValidation.js"></script>
+    <script src="assets/js/cadastro.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="assets/js/agency.min.js"></script>
 </body>
 
 </html>
