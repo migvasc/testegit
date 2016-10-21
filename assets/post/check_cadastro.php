0735275a5281df6a28f1bd43eb0a2813de7c0b10
migvasc <miguel.vasconcelos@usp.br>
2016-10-21 01:52:10 +0000

    implementando cadastro no bd 7 - faltou um parentese na qery

--- assets/post/check_cadastro.php
+++ assets/post/check_cadastro.php
@@ -1,7 +1,7 @@
 <?php
 
 require("../../conn.php");
-$result = pg_query($conn, "insert into usuario values ('". htmlentities($_POST['email'], ENT_QUOTES, "UTF-8")."','" . htmlentities($_POST['senha1'], ENT_QUOTES, "UTF-8") ."','". htmlentities($_POST['nome'], ENT_QUOTES, "UTF-8") ."','". htmlentities($_POST['sobrenome'], ENT_QUOTES, "UTF-8") ."','". htmlentities($_POST['phone'], ENT_QUOTES, "UTF-8") ."'");
+$result = pg_query($conn, "insert into usuario values ('". htmlentities($_POST['email'], ENT_QUOTES, "UTF-8")."','" . htmlentities($_POST['senha1'], ENT_QUOTES, "UTF-8") ."','". htmlentities($_POST['nome'], ENT_QUOTES, "UTF-8") ."','". htmlentities($_POST['sobrenome'], ENT_QUOTES, "UTF-8") ."','". htmlentities($_POST['phone'], ENT_QUOTES, "UTF-8") ."')");
  
 if(pg_affected_rows($result) > 0)
 	echo (pg_affected_rows($result));
