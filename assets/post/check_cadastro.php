5f08c18f547c17de9917ff215e1a908bc54002fc
migvasc <miguel.vasconcelos@usp.br>
2016-10-21 02:09:43 +0000

    implementando cadastro no bd 9 - alterando mensagem pos-cadastro (aparecer o nome)

--- assets/post/check_cadastro.php
+++ assets/post/check_cadastro.php
@@ -3,8 +3,12 @@
 require("../../conn.php");
 $result = pg_query($conn, "insert into usuario values ('". htmlentities($_POST['email'], ENT_QUOTES, "UTF-8")."','" . htmlentities($_POST['senha1'], ENT_QUOTES, "UTF-8") ."','". htmlentities($_POST['nome'], ENT_QUOTES, "UTF-8") ."','". htmlentities($_POST['sobrenome'], ENT_QUOTES, "UTF-8") ."','". htmlentities($_POST['phone'], ENT_QUOTES, "UTF-8") ."')");
  
-if(pg_affected_rows($result) > 0)
-	echo (pg_affected_rows($result));
+if(pg_affected_rows($result) > 0){
+	$result = pg_query($conn, "select nome from usuario where email = '". htmlentities($_POST['email'], ENT_QUOTES, "UTF-8")."'");
+	echo (pg_fetch_row($result)[0]);
+}
+
+	
 else
 	echo null;
 ?>
