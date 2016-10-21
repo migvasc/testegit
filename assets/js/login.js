950298cd533faa5e1b2620f2fcfd1db4a1206111
migvasc <miguel.vasconcelos@usp.br>
2016-10-21 04:52:32 +0000

    adicionando redirecionamento após login

--- assets/js/login.js
+++ assets/js/login.js
@@ -24,13 +24,14 @@ $(function() {
                 success: function(resposta) {
                     if(resposta != null && resposta != ""){
                         alert("Bem-vindo(a), "+resposta);
-                        $('#success').html("<div class='alert alert-success'>");
+                        /*$('#success').html("<div class='alert alert-success'>");
                         $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
                         $('#success > .alert-success')
                             .append("<strong>Usuário entrou no sistema com sucesso. </strong>");
                         $('#success > .alert-success')
-                            .append('</div>');
+                            .append('</div>');*/
+                        window.location.assign("home.php");
                     }
                         
                     else{
