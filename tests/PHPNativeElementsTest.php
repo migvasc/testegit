06d850298a634e699f97067368794df8afdad36e
migvasc <miguel.vasconcelos@usp.br>
2016-10-24 21:03:59 +0000

    testando jasmine 1

---
+++ tests/PHPNativeElementsTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use PHPUnit_Framework_TestCase as PHPUnit;
+
+class PHPNativeElementsTest extends PHPUnit{
+
+	public function setUp(){
+
+
+	}
+
+	public function tearDown(){
+
+
+
+
+	}
+
+
+
+
+}
+
+
+
+
+
