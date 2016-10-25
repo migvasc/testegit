db7a3bcb2c815ef04de2e55a0a012e1004fc1bbd
migvasc <miguel.vasconcelos@usp.br>
2016-10-25 03:13:23 +0000

    testando retorno ajax 12

--- teste/check.js
+++ teste/check.js
@@ -34,7 +34,7 @@ describe('testAle', function() {
     var email = 'teste@teste.com';
     var senha = 'admin123';
     $.ajax({
-        url: "assets/post/check_login.php",
+        url: "../post/check_login.php",
         type: "POST",
         data: {
             email: email,
