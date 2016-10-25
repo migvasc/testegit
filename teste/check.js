936d072bb520190f384e6dd64db38720cd6340ac
migvasc <miguel.vasconcelos@usp.br>
2016-10-25 03:52:26 +0000

    testando retorno ajax 23

--- teste/check.js
+++ teste/check.js
@@ -34,8 +34,8 @@ describe('testAle', function() {
     var email = 'teste@teste.com';
     var senha = 'admin123';
     $.ajax({
-        //url: "http://localhost/assets/post/check_login.php",
-        url: "http://localhost/petajuda/assets/post/check_login.php",
+        url: "http://localhost/assets/post/check_login.php",
+        //url: "http://localhost/petajuda/assets/post/check_login.php",
         type: "POST",
         data: {
             email: email,
