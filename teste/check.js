63a20ab364c031af0784c5e44b297d2ed9a4b47d
migvasc <miguel.vasconcelos@usp.br>
2016-10-25 00:03:43 +0000

    testando retorno ajax

--- teste/check.js
+++ teste/check.js
@@ -1,7 +1,26 @@
-describe('getDiv', function() {
-    var d = document.querySelector('.box');
+//describe('getDiv', function() {
+//    var d = document.querySelector('.box');
 
-    it('Should exist', function() {
-        expect(d.nodeName).toBe('DIV');
+//    it('Should exist', function() {
+//        expect(d.nodeName).toBe('DIV');
+//    });
+//});
+
+describe('Ajax login tests', function() {
+    
+     it('should receive a successful response', function() {
+        spyOn($, "ajax").andCallFake(function(e) {
+        e.success({});
     });
+    
+    var username = 'teste@teste.com';
+    var senha = 'admin123'
+    
+    getUserName(username,senha);
+    
+    expect($.ajax.mostRecentCall.args[0]["url"]).toEqual("assets/post/check_login.php");
+    
+    //sendRequest(callbacks, configuration);
+    //expect(callbacks.checkForInformation).toHaveBeenCalled();  //Verifies this was called
+    //expect(callbacks.displayErrorMessage).not.toHaveBeenCalled(); 
 });
