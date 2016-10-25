3791b23e90d46aee4cfac36de0599e762e48bad7
migvasc <miguel.vasconcelos@usp.br>
2016-10-25 02:00:32 +0000

    testando retorno ajax 4

--- teste/check.js
+++ teste/check.js
@@ -6,22 +6,48 @@
 //    });
 //});
 
-describe('Ajax login tests', function() {
+//describe('Ajax login tests', function() {
     
-     it('should receive a successful response', function() {
-        spyOn($, "ajax").andCallFake(function(e) {
-        e.success({});
-        });
-     });
+//     it('should receive a successful response', function() {
+//        spyOn($, "ajax").andCallFake(function(e) {
+//        e.success({});
+//        });
+//    });
     
-    var username = 'teste@teste.com';
-    var senha = 'admin123';
+//    var username = 'teste@teste.com';
+//    var senha = 'admin123';
     
-    getUserName(username,senha);
+//    getUserName(username,senha);
     
-    expect($.ajax.mostRecentCall.args[0]["url"]).toEqual("assets/post/check_login.php");
+//    expect($.ajax.mostRecentCall.args[0]["url"]).toEqual("assets/post/check_login.php");
     
     //sendRequest(callbacks, configuration);
     //expect(callbacks.checkForInformation).toHaveBeenCalled();  //Verifies this was called
     //expect(callbacks.displayErrorMessage).not.toHaveBeenCalled(); 
+//});
+
+
+it("shoul fail", function () {
+    var callback = jasmine.createSpy();
+    getUserName(email, senha, callback);
+    waitsFor(function() {
+        return callback.callCount > 0;
+    });
+    runs(function() {
+        expect(callback).toHaveBeenCalled();
+    });
 });
+
+function getUserName(email, senha, callback){
+    $.ajax({
+                url: "assets/post/check_login.php",
+                type: "POST",
+                data: {
+                    email: email,
+                    senha: senha
+                },
+                cache: false,
+                success: callback
+                },
+            });
+}
