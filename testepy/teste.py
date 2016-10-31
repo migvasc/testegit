1c42ee468a18154b1756d392f1e80e970df1a706
migvasc <miguel.vasconcelos@usp.br>
2016-10-31 22:46:58 +0000

    TESTANDO SAUCE 19

--- testepy/teste.py
+++ testepy/teste.py
@@ -1,76 +1,126 @@
+#!/usr/bin/python
+
+import os
+import sys
+import new
+from random import randint
+import base64
+import json
+import httplib
 import unittest
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-#from sauceclient import SauceClient
-import sauceclient
-import os
+from selenium.webdriver.support.ui import WebDriverWait
 
-class AskGitHubTestSuite(unittest.TestCase):
+from data import SUPPORTED, UNSUPPORTED
+
+
+class CssifyTest(unittest.TestCase):
+    __test__ = False
+
     def setUp(self):
-        #Sauce Labs
-        self.desired_cap = {
-        #'platform': "Mac OS X 10.9",
-        'tunnel-identifier': os.environ.get('TRAVIS_JOB_NUMBER'), #IMPORTANT!!!
-        'browserName': "chrome",
-        'build': os.environ.get('TRAVIS_BUILD_NUMBER'),
-        'name': "AskGitHub: Latest from GitHub",
-        'tags': [ "python","flask","mongo","neo4j","github"],
-        'username': os.environ.get('SauceLogin'),
-        'accessKey': os.environ.get('SauceAccessKey'),
-        }
-        self.SauceLogin = os.environ['SauceLogin']
-        self.SauceAccessKey = os.environ['SauceAccessKey']
-        self.driver = webdriver.Remote(command_executor="http://%s:%s@ondemand.saucelabs.com:80/wd/hub" 
-                      % (self.SauceLogin, self.SauceAccessKey),desired_capabilities=self.desired_cap)
-        #self.URL = "http://askgithub.com"
-        # Travis-CI & Sauce Labs
-        self.URL = "http://localhost:" + os.environ.get('PORT') 
-        self.driver.get(self.URL)
-        self.status = True
-        self.message = ""
+        self.caps['name'] = 'Testing cssify'
+        if (os.environ.get('TRAVIS') and
+            os.environ.get('HAS_JOSH_K_SEAL_OF_APPROVAL')):
+            self.caps['tunnel-identifier'] = os.environ['TRAVIS_JOB_NUMBER']
+            self.caps['build'] = os.environ['TRAVIS_BUILD_NUMBER']
+            self.caps['tags'] = [os.environ['TRAVIS_PYTHON_VERSION'], 'CI']
+        self.url = 'http://localhost:8080/'
 
-    def test_suite(self):
-        print("Test 1: Smoke test")
-        try:    
-            assert "Ask GitHub" in self.driver.title
-        except:
-            self.message = "Test 1 failed."
-            self.status = False
-        #
-        print("Test 2: Trending now")    
-        elem = self.driver.find_element_by_name("q")
-        elem.send_keys("trending now")
-        elem.send_keys(Keys.RETURN)
-        try:
-            assert "No results found" not in self.driver.page_source
-        except:
-            self.message += "Test 2 failed."
-            self.status = False
-        #
-        print("Test 3: Top repositories")
-        elem = self.driver.find_element_by_name("q")    
-        elem.send_keys("top repositories")
-        elem.send_keys(Keys.RETURN)
-        elem = self.driver.find_elements_by_class_name("repositoryinfo")
-        try:
-            assert len(elem) > 0   #minimum 1 result
-            elem = self.driver.find_element_by_class_name("repositoryinfo")
-            elem.click() #click first link
-            assert "No results found" not in self.driver.page_source
-        except:
-            self.message += "Test 3 failed."
-            self.status = False   
+        self.username = os.environ['SAUCE_USERNAME']
+        self.key = os.environ['SAUCE_ACCESS_KEY']
+        hub_url = "%s:%s@localhost:4445" % (self.username, self.key)
+        self.driver = webdriver.Remote(desired_capabilities=self.caps,
+                                       command_executor="http://%s/wd/hub" % hub_url)
+        self.jobid = self.driver.session_id
+        print "Sauce Labs job: https://saucelabs.com/jobs/%s" % self.jobid
+        self.driver.implicitly_wait(30)
 
+    def report_test_result(self):
+        base64string = base64.encodestring('%s:%s'
+                                           % (self.username, self.key))[:-1]
+        result = json.dumps({'passed': sys.exc_info() == (None, None, None)})
+        connection = httplib.HTTPConnection("saucelabs.com")
+        connection.request('PUT',
+                           '/rest/v1/%s/jobs/%s' % (self.username, self.jobid),
+                           result,
+                           headers={"Authorization": "Basic %s" % base64string})
+        result = connection.getresponse()
+        return result.status == 200
+
+    def test_supported_cssify(self):
+        for path, cssified in SUPPORTED:
+            self.driver.get(self.url)
+            xpath = self.driver.find_element_by_id('xpath')
+            xpath.send_keys(path)
+            xpath.submit()
+            css = WebDriverWait(self.driver, 30).until(
+                lambda driver: driver.find_element_by_id("css"))
+            self.assertEqual(css.get_attribute("value"), cssified)
+
+    def test_unsupported_cssify(self):
+        for path in UNSUPPORTED:
+            self.driver.get(self.url)
+            self.assertTrue("cssify" in self.driver.title)
+            xpath = self.driver.find_element_by_id('xpath')
+            xpath.send_keys(path)
+            xpath.submit()
+            fail = WebDriverWait(self.driver, 30).until(
+                lambda driver: driver.find_element_by_class_name("fail"))
+            self.assertEqual("Invalid or unsupported Xpath: %s" % path,
+                             fail.text)
 
     def tearDown(self):
-        print("testing complete;closing connection")
-        id = self.driver.session_id
-        print("Link to job: https://saucelabs.com/jobs/%s" % (id))
-        print("Errors?", self.message)
-        sauce_client = SauceClient(self.SauceLogin, self.SauceAccessKey)
-        sauce_client.jobs.update_job(self.driver.session_id, passed=self.status)
         self.driver.quit()
+        self.report_test_result()
 
-if __name__ == "__main__":
-    unittest.main()
+
+PLATFORMS = [
+    {'browserName': 'firefox',
+     'version': '19',
+     'platform': 'LINUX',
+     },
+    {'browserName': 'firefox',
+     'version': '19',
+     'platform': 'XP',
+     },
+    {'browserName': 'firefox',
+     'version': '19',
+     'platform': 'VISTA',
+     },
+    {'browserName': 'chrome',
+     'platform': 'LINUX',
+     },
+    {'browserName': 'chrome',
+     'platform': 'XP',
+     },
+    {'browserName': 'chrome',
+     'platform': 'VISTA',
+     },
+    {'browserName': 'internet explorer',
+     'version': '10',
+     'platform': 'WIN8',
+     },
+    {'browserName': 'internet explorer',
+     'version': '9',
+     'platform': 'VISTA',
+     },
+    {'browserName': 'internet explorer',
+     'version': '8',
+     'platform': 'XP',
+     },
+]
+
+classes = {}
+for platform in PLATFORMS:
+    d = dict(CssifyTest.__dict__)
+    name = "%s_%s_%s_%s" % (CssifyTest.__name__,
+                            platform['browserName'],
+                            platform.get('platform', 'ANY'),
+                            randint(0, 999))
+    name = name.replace(" ", "").replace(".", "")
+    d.update({'__test__': True,
+              'caps': platform,
+              })
+    classes[name] = new.classobj(name, (CssifyTest,), d)
+
+globals().update(classes)
