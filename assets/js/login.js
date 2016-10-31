db100d29f015bc6f2be7fb5f079e2c657384e455
migvasc <miguel.vasconcelos@usp.br>
2016-10-31 23:51:58 +0000

    TESTANDO SAUCE 37

--- assets/js/login.js
+++ assets/js/login.js
@@ -26,7 +26,7 @@ $(function () {
                         $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
                         $('#success > .alert-success')
-                            .append("<strong>Usuário entrou no sistema com sucesso. </strong>");
+                            .append("<strong>Usuário entrou no sistema. </strong>");
                         $('#success > .alert-success')
                             .append('</div>');
                     //    alert("Bem-vindo(a), "+resposta);
