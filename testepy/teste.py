fcdc911d4244cff6b1f0482bec4ce3ea17a81944
migvasc <miguel.vasconcelos@usp.br>
2016-10-31 22:41:15 +0000

    TESTANDO SAUCE 18

--- testepy/teste.py
+++ testepy/teste.py
@@ -2,7 +2,8 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from sauceclient import SauceClient
+#from sauceclient import SauceClient
+import sauceclient
 import os
 
 class AskGitHubTestSuite(unittest.TestCase):
