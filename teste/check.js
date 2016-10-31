e86d4aa90c3f2786d234aedd6eeaaec5253b8b67
migvasc <miguel.vasconcelos@usp.br>
2016-10-31 13:14:06 +0000

    testando retorno ajax 40 - testando spyOn 3

--- teste/check.js
+++ teste/check.js
@@ -71,12 +71,13 @@ describe('testAle', function() {
 });*/
 
 
-
-describe("Ajax Tests", function() {
+// Desse jeito nao funciona porque parece uqe o ajax nao eh chamado
+/*describe("Ajax Tests", function() {
     var configuration = { url: "http://localhost/assets/post/check_login.php",
                           email: "teste@tes.com",
                           senha: "admin123"
                         };
+    // aqui nao funciona porque a funcao andcallFake nao esta definida
     it("should make an Ajax request to the correct URL", function() {
         spyOn($, "ajax").andCallFake(function(e) {
             e.success({});
@@ -92,6 +93,7 @@ describe("Ajax Tests", function() {
         expect(callbacks.displayErrorMessage).not.toHaveBeenCalled();  //Verifies this was NOT called
     });
     
+    //aqui nao funciona porque o ajax parece nao ser chamado (resposta = "nope")
     //it("should make an Ajax request to the correct URL", function() {
     //    spyOn($, "ajax");
     //    var x = sendRequest(undefined, configuration);
@@ -122,11 +124,52 @@ function sendRequest(callbacks, configuration) {
                 x = xhr.statusText;
             }
         }
-        /*error: function(data) {
-            x = "nopenope";
-            callbacks.displayErrorMessage();
-        }*/
+        //error: function(data) {
+        //    x = "nopenope";
+        //    callbacks.displayErrorMessage();
+        //}
         
     });
     return x;
+}*/
+
+describe("A jQuery ajax request should be able to fetch...", function() {
+    it("should make an AJAX request to the correct URL", function() {
+        spyOn($, "ajax");
+        getUserName("teste@teste.com", "admin123");
+        expect($.ajax.mostRecentCall.args[0]["url"]).toEqual("http://localhost/assets/post/check_login.php");
+    });
+});
+
+function getUserName(email, senha) {
+    var a = "nope"
+   $.ajax({
+        url: "http://localhost/assets/post/check_login.php",
+        //url: "assets/post/check_login.php",
+        type: "POST",
+        data: {
+            email: email,
+            senha: senha
+        },
+        cache: false,
+        async: false,
+        success: function(resposta) {
+            if(resposta != null && resposta != ""){
+                //usuario+senha = valido
+                a = "yep";
+            }
+            else{
+                //usuario+senha = invalido
+                a = "nope1";
+            }
+        },
+        error: function (xhr, ajaxOptions, thrownError){
+            if(xhr.status==404) {
+                a = "nope404";
+            }
+            else {
+                a = xhr.statusText;
+            }
+        }
+    });
 }
