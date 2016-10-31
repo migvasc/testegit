d8d81db3cd46025c15d09784e727d6c13c19d8f7
migvasc <miguel.vasconcelos@usp.br>
2016-10-31 22:50:46 +0000

    TESTANDO SAUCE 19

--- testepy/teste.py
+++ testepy/teste.py
@@ -1,126 +1,29 @@
-#!/usr/bin/python
-
-import os
-import sys
-import new
-from random import randint
-import base64
-import json
-import httplib
-import unittest
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-
-from data import SUPPORTED, UNSUPPORTED
-
-
-class CssifyTest(unittest.TestCase):
-    __test__ = False
-
-    def setUp(self):
-        self.caps['name'] = 'Testing cssify'
-        if (os.environ.get('TRAVIS') and
-            os.environ.get('HAS_JOSH_K_SEAL_OF_APPROVAL')):
-            self.caps['tunnel-identifier'] = os.environ['TRAVIS_JOB_NUMBER']
-            self.caps['build'] = os.environ['TRAVIS_BUILD_NUMBER']
-            self.caps['tags'] = [os.environ['TRAVIS_PYTHON_VERSION'], 'CI']
-        self.url = 'http://localhost:8080/'
-
-        self.username = os.environ['SAUCE_USERNAME']
-        self.key = os.environ['SAUCE_ACCESS_KEY']
-        hub_url = "%s:%s@localhost:4445" % (self.username, self.key)
-        self.driver = webdriver.Remote(desired_capabilities=self.caps,
-                                       command_executor="http://%s/wd/hub" % hub_url)
-        self.jobid = self.driver.session_id
-        print "Sauce Labs job: https://saucelabs.com/jobs/%s" % self.jobid
-        self.driver.implicitly_wait(30)
-
-    def report_test_result(self):
-        base64string = base64.encodestring('%s:%s'
-                                           % (self.username, self.key))[:-1]
-        result = json.dumps({'passed': sys.exc_info() == (None, None, None)})
-        connection = httplib.HTTPConnection("saucelabs.com")
-        connection.request('PUT',
-                           '/rest/v1/%s/jobs/%s' % (self.username, self.jobid),
-                           result,
-                           headers={"Authorization": "Basic %s" % base64string})
-        result = connection.getresponse()
-        return result.status == 200
-
-    def test_supported_cssify(self):
-        for path, cssified in SUPPORTED:
-            self.driver.get(self.url)
-            xpath = self.driver.find_element_by_id('xpath')
-            xpath.send_keys(path)
-            xpath.submit()
-            css = WebDriverWait(self.driver, 30).until(
-                lambda driver: driver.find_element_by_id("css"))
-            self.assertEqual(css.get_attribute("value"), cssified)
-
-    def test_unsupported_cssify(self):
-        for path in UNSUPPORTED:
-            self.driver.get(self.url)
-            self.assertTrue("cssify" in self.driver.title)
-            xpath = self.driver.find_element_by_id('xpath')
-            xpath.send_keys(path)
-            xpath.submit()
-            fail = WebDriverWait(self.driver, 30).until(
-                lambda driver: driver.find_element_by_class_name("fail"))
-            self.assertEqual("Invalid or unsupported Xpath: %s" % path,
-                             fail.text)
-
-    def tearDown(self):
-        self.driver.quit()
-        self.report_test_result()
-
-
-PLATFORMS = [
-    {'browserName': 'firefox',
-     'version': '19',
-     'platform': 'LINUX',
-     },
-    {'browserName': 'firefox',
-     'version': '19',
-     'platform': 'XP',
-     },
-    {'browserName': 'firefox',
-     'version': '19',
-     'platform': 'VISTA',
-     },
-    {'browserName': 'chrome',
-     'platform': 'LINUX',
-     },
-    {'browserName': 'chrome',
-     'platform': 'XP',
-     },
-    {'browserName': 'chrome',
-     'platform': 'VISTA',
-     },
-    {'browserName': 'internet explorer',
-     'version': '10',
-     'platform': 'WIN8',
-     },
-    {'browserName': 'internet explorer',
-     'version': '9',
-     'platform': 'VISTA',
-     },
-    {'browserName': 'internet explorer',
-     'version': '8',
-     'platform': 'XP',
-     },
-]
-
-classes = {}
-for platform in PLATFORMS:
-    d = dict(CssifyTest.__dict__)
-    name = "%s_%s_%s_%s" % (CssifyTest.__name__,
-                            platform['browserName'],
-                            platform.get('platform', 'ANY'),
-                            randint(0, 999))
-    name = name.replace(" ", "").replace(".", "")
-    d.update({'__test__': True,
-              'caps': platform,
-              })
-    classes[name] = new.classobj(name, (CssifyTest,), d)
-
-globals().update(classes)
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+  
+# This is the only code you need to edit in your existing scripts.
+# The command_executor tells the test to run on Sauce, while the desired_capabilities
+# parameter tells us which browsers and OS to spin up.
+desired_cap = {
+    'platform': "Mac OS X 10.9",
+    'browserName': "chrome",
+    'version': "31",
+}
+driver = webdriver.Remote(
+   command_executor='http://YOUR_SAUCE_USERNAME:YOUR_SAUCE_ACCESS_KEY@ondemand.saucelabs.com:80/wd/hub',
+   desired_capabilities=desired_cap)
+  
+# This is your test logic. You can add multiple tests here.
+driver.implicitly_wait(10)
+driver.get("http://www.google.com")
+if not "Google" in driver.title:
+    raise Exception("Unable to load google page!")
+elem = driver.find_element_by_name("q")
+elem.send_keys("Sauce Labs")
+elem.submit()
+print driver.title
+  
+# This is where you tell Sauce Labs to stop running tests on your behalf.
+# It's important so that you aren't billed after your test finishes.
+driver.quit()
