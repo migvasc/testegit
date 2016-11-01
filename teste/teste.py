f4a425af4470544accab7269664ffccff39e93fb
migvasc <miguel.vasconcelos@usp.br>
2016-11-01 02:12:57 +0000

    adicionando testes

--- teste/teste.py
+++ teste/teste.py
@@ -15,7 +15,7 @@ driver = webdriver.Remote(
    command_executor='http://migvasc:edb8e905-bb7c-45f0-8c81-fa2a41402c5d@ondemand.saucelabs.com:80/wd/hub',
    desired_capabilities=desired_cap)
   
-def login_sucesso():
+def login_sucess():
     driver.implicitly_wait(10)
     driver.get("http://petajuda.herokuapp.com/login.php")
     driver.find_element_by_id("email").click()
@@ -27,7 +27,50 @@ def login_sucesso():
     if not "Pet Ajuda | Home" in driver.title:
         raise Exception("Unable to load google page!")
     print driver.title
+    
+def login_fail_emptyEmail():
+    driver.implicitly_wait(10)
+    driver.get("http://petajuda.herokuapp.com/login.php")
+    driver.find_element_by_id("email").click()
+    driver.find_element_by_id("email").clear()
+    driver.find_element_by_id("email").send_keys("")
+    driver.find_element_by_id("senha").clear()
+    driver.find_element_by_id("senha").send_keys("1234")
+    driver.find_element_by_xpath("//button[@type='submit']").click()
+    if not "Pet Ajuda | Login" in driver.title:
+        raise Exception("Unable to load google page!")
+    print driver.title
+    
+def login_fail_emptySenha():
+    driver.implicitly_wait(10)
+    driver.get("http://petajuda.herokuapp.com/login.php")
+    driver.find_element_by_id("email").click()
+    driver.find_element_by_id("email").clear()
+    driver.find_element_by_id("email").send_keys("carlos@carlos.com")
+    driver.find_element_by_id("senha").clear()
+    driver.find_element_by_id("senha").send_keys("")
+    driver.find_element_by_xpath("//button[@type='submit']").click()
+    if not "Pet Ajuda | Login" in driver.title:
+        raise Exception("Unable to load google page!")
+    print driver.title
+
+def login_fail_emptyEmail_emptySenha():
+    driver.implicitly_wait(10)
+    driver.get("http://petajuda.herokuapp.com/login.php")
+    driver.find_element_by_id("email").click()
+    driver.find_element_by_id("email").clear()
+    driver.find_element_by_id("email").send_keys("")
+    driver.find_element_by_id("senha").clear()
+    driver.find_element_by_id("senha").send_keys("")
+    driver.find_element_by_xpath("//button[@type='submit']").click()
+    if not "Pet Ajuda | Login" in driver.title:
+        raise Exception("Unable to load google page!")
+    print driver.title
 
 
-login_sucesso()
+login_sucess()
+login_fail_emptyEmail()
+login_fail_emptySenha()
+login_fail_emptyEmail_emptySenha()
+
 driver.quit()
