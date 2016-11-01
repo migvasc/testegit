2abfd5a73dd36394a8e111d89953af686b0a7299
migvasc <miguel.vasconcelos@usp.br>
2016-11-01 05:25:03 +0000

    alterando menu como pag externa em todas as pag

--- cadastro.php
+++ cadastro.php
@@ -37,39 +37,7 @@
 
 <body id="page-top" class="index">
 
-<!-- Navigation -->
-    <nav id="mainNav" class="navbar navbar-default navbar-custom navbar-fixed-top">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header page-scroll">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
-                </button>
-                <a class="navbar-brand page-scroll" href="index.php#page-top"><img src="assets/images/logo_extenso.png" alt="Progressus HTML5 template"></a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="hidden">
-                        <a href="#page-top"></a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="index.php#about">About</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="index.php#team">Team</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="index.php#contact">Contact</a>
-                    </li>
-                    <li><a href="login.php" class="page-scroll btn btn-xl">SIGN IN / SIGN UP</a></li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container-fluid -->
-    </nav>
+    require("menu.php");
 
     <div class="container-fluid full-height" id="fundo-login">
             <div class="container full-height">
