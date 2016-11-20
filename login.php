b003c8bd90ad1d7666558a8681bc23ec495663b6
migvasc <miguel.vasconcelos@usp.br>
2016-11-20 02:36:20 +0000

    tentando sessao de novo 22 - adicionando o teste de sessao na pagina d elogin

--- login.php
+++ login.php
@@ -11,6 +11,19 @@
     <meta http-equiv = "Content-Type" content = "text/html; charset=utf-8" />
 
     <title>Pet Ajuda | Login</title>
+    
+    <?php 
+//        echo isset($_SESSION);
+  //      die(); 
+        session_start();
+        if(isset($_SESSION['user'])) { //se sessão nao existe
+             //ve se pode criar uma
+            header("Location: home/index.php");
+            exit();
+        }
+        
+    ?>
+
 
     <!-- Bootstrap Core CSS -->
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
