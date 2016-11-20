1201892c3d762c78e65f8d960eedeb013a70a527
migvasc <miguel.vasconcelos@usp.br>
2016-11-20 14:57:25 +0000

    Teste redirecionamento para HOME 5 (desfeito)

--- teste/teste.py
+++ teste/teste.py
@@ -252,22 +252,13 @@ def signup_fail_userAlreadyRegistered():
     if not "Pet Ajuda | Cadastro" in driver.title:
         raise Exception("Cadastro com email vazio passando!")
     print "Teste signup_fail_userAlreadyRegistered - done"
-'''    
-def redirect_home():
-    driver.implicitly_wait(10)
-    driver.get("http://petajuda.herokuapp.com/login.php")
-    driver.implicitly_wait(10)
-    if not "Pet Ajuda | Home" in driver.title:
-        raise Exception("Algo errado com a sessão!")
-    print "Teste redirect_home - done"    
-'''
+
 login_fail_emptyEmail()
 login_fail_emptySenha()
 login_fail_emptyEmail_emptySenha()
 login_fail_wrongEmail()
 login_fail_wrongSenha()
 login_sucess()
-#redirect_home()
 
 logout()
 
