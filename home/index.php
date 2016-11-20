51bf3a1014913e78949e421844807090ae5a5952
migvasc <miguel.vasconcelos@usp.br>
2016-11-20 02:29:12 +0000

    tentando sessao de novo 22 - adicionando o nome do usuario na barrinha

--- home/index.php
+++ home/index.php
@@ -229,7 +229,7 @@
                             <span class="profile-ava">
                                 <img alt="" src="img/avatar1_small.jpg">
                             </span>
-                            <span class="username">Teste</span>
+                            <span class="username"><?php echo($_SESSION['user']); ?></span>
                             <b class="caret"></b>
                         </a>
                         <ul class="dropdown-menu extended logout">
