24893b84db97b70386cf591eaf5fb93bb0bb801a
migvasc <miguel.vasconcelos@usp.br>
2016-11-21 00:26:08 +0000

    acrescentando perfil php

---
+++ assets/post/generate_json.php
@@ -0,0 +1,15 @@
+<?php
+
+require("../../conn.php");
+
+$result = pg_query($conn, "select row_to_json(usuario_teste) from usuario_teste;");
+
+
+if(pg_num_rows($result)){
+	echo (pg_fetch_row($result)[0]);
+	$myfile = fopen("newfile.json", "w") or die("Unable to open file!"); 
+	fwrite($myfile,$result);
+}
+else
+	echo null;
+?>
