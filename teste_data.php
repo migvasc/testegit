b462ba52e140abf8d953df635cb3741fa8bbd11d
migvasc <miguel.vasconcelos@usp.br>
2016-11-21 14:58:43 +0000

    TESTANDO PHP JS 24

--- teste_data.php
+++ teste_data.php
@@ -6,16 +6,20 @@ $result = pg_query($conn, "select row_to_json(usuario_teste) from usuario_teste;
 
 
 if(pg_num_rows($result)){
-    
+    $myarray = array()
+    while ($row = pg_fetch_row($contests)) {
+        $myarray[] = $row;
+    }
+
+    echo json_encode($myarray);
 //	echo (pg_fetch_row($result)[0]);
 //	$myfile = fopen("newfile.json", "w") or die("Unable to open file!"); 
 //fwrite($myfile,$result);
-    echo json_encode($result);
+    
     //teste
 }
 else{
 	echo json_encode(24);
-    
     
 }
 ?>
