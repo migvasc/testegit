cfcfcb73954e07919322a312f6f43415e4ea58b2
migvasc <miguel.vasconcelos@usp.br>
2016-11-21 20:11:55 +0000

    testando retorno json com endereco 18

--- mapa2.js
+++ mapa2.js
@@ -15,14 +15,29 @@ function initialize() {
     map = new google.maps.Map(document.getElementById("mapa"), options);
  
     geocoder = new google.maps.Geocoder();
- 
-
 }
  
-    function carregarNoMapa(pontos) {
+function carregarNoMapa(pontos) {
     
-    console.log("Pontos na funcao carregarNoMapa:" +pontos);
-	//para cada ponto no json, carrega as coordenadas
+    for(var ponto in pontos){
+        console.log("Ponto na funcao carregarNoMapa:" +ponto);
+        geocoder.geocode({ 'address': ponto['endereco_logradouro'] +', '+ponto['endereco_numero']+', '+ ponto['endereco_cidade']+' '+ ', Brasil', 'region': 'BR' }, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                if (results[0]) {
+                    var latitude = results[0].geometry.location.lat();
+                    var longitude = results[0].geometry.location.lng(); 
+    	            var marker = new google.maps.Marker({
+    	                position: new google.maps.LatLng(latitude, longitude),
+    	                title: "Meu ponto personalizado! :-D",
+    	                map: map,
+        		        icon: 'marcador.png'
+    	            });
+                }
+            }
+        });
+    }
+    
+	/*//para cada ponto no json, carrega as coordenadas
         $.each(pontos, function(index, ponto) {
             console.log("Ponto na funcao carregarNoMapa:" +ponto);
     
@@ -43,11 +58,8 @@ function initialize() {
             }
         });
  
-       });
- 
-
-
-    }
+       });*/
+}
 
 function run(pontos){
     initialize(); 	
@@ -57,32 +69,28 @@ function run(pontos){
 
 function reqListener () {
         
-      console.log(this.responseText);
-      
-      
-    }
+    console.log(this.responseText);
+}
 
-    var oReq = new XMLHttpRequest(); //New request object
+var oReq = new XMLHttpRequest(); //New request object
     
-    oReq.onload = function() {
+oReq.onload = function() {
         
-        //This is where you handle what to do with the response.
-        //The actual data is found on this.responseText
-        
-        alert(this.responseText);
-        
-        var array = JSON.parse(this.responseText);
-        
-        console.log(array[0]);
-        
-        run(array);
-        
-        
-    };
+    //This is where you handle what to do with the response.
+    //The actual data is found on this.responseText
+    
+    alert(this.responseText);
+    
+    var array = JSON.parse(this.responseText);
+    
+    console.log(array[0]);
+    
+    run(array);
+};
     
     
-    oReq.open("get", "teste_data.php", true);
-    //                               ^ Don't block the rest of the execution.
-    //                                 Don't wait until the request finishes to 
-    //                                 continue.
-    oReq.send();
+oReq.open("get", "teste_data.php", true);
+//                               ^ Don't block the rest of the execution.
+//                                 Don't wait until the request finishes to 
+//                                 continue.
+oReq.send();
