7f7066d68dec69c0392e741fc6aa58f72a739024
migvasc <miguel.vasconcelos@usp.br>
2016-11-21 18:08:17 +0000

    testando retorno json com endereco 6

--- teste_data.php
+++ teste_data.php
@@ -14,7 +14,7 @@ if(pg_num_rows($result)){
     // $decoded_array = json_decode($resultArray[]0['rua']);
     
     // echo json_encode($decoded_array, JSON_PRETTY_PRINT);
-    echo [$result_array[0],$result_array[1], $result_array[2],$result_array[3]];
+    echo json_encode([$result_array[0],$result_array[1], $result_array[2],$result_array[3]], JSON_PRETTY_PRINT);
     
 }
 //	echo (pg_fetch_row($result)[0]);
