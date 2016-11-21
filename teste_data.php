4dd46780c10990d1ebe2065c00c5b308a14dbcc5
migvasc <miguel.vasconcelos@usp.br>
2016-11-21 01:00:29 +0000

    TESTE PHP E JS 2

--- teste_data.php
+++ teste_data.php
@@ -1,1 +1,17 @@
-echo json_encode(42);
+<?php
+
+require("../../conn.php");
+
+$result = pg_query($conn, "select row_to_json(usuario_teste) from usuario_teste;");
+
+
+if(pg_num_rows($result)){
+//	echo (pg_fetch_row($result)[0]);
+//	$myfile = fopen("newfile.json", "w") or die("Unable to open file!"); 
+//fwrite($myfile,$result);
+    echo json_encode($result);
+}
+else
+	echo null;
+?>
+
