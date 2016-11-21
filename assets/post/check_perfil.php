0aac02df3170923b0e8bc0e90950d8ca0964cb6d
migvasc <miguel.vasconcelos@usp.br>
2016-11-21 04:21:09 +0000

    criando pagina de perfil 23

--- assets/post/check_perfil.php
+++ assets/post/check_perfil.php
@@ -1,7 +1,7 @@
 <?php
 
 require("../../conn.php");
-$result = pg_query($conn, "update usuario set nome='". $_POST['nome'] ."', sobrenome ='". $_POST['sobrenome'] ."', telefone = '". $_POST['telefone'] ."', resposta = '". $_POST['resposta'] . "'  where email = '". $_POST['email'] ."'");
+$result = pg_query($conn, "update usuario set nome='". $_POST['nome'] ."', sobrenome ='". $_POST['sobrenome'] ."', phone = '". $_POST['telefone'] ."', resposta = '". $_POST['resposta'] . "'  where email = '". $_POST['email'] ."'");
  
 if(pg_affected_rows($result) > 0){
 	$result = pg_query($conn, "select nome from usuario where email = '". htmlentities($_POST['email'], ENT_QUOTES, "UTF-8")."'");
