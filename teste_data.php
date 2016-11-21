b942efc0de455608c381e191a741f6f477cd3326
migvasc <miguel.vasconcelos@usp.br>
2016-11-21 15:28:46 +0000

    TESTANDO PHP JS 28

--- teste_data.php
+++ teste_data.php
@@ -9,7 +9,7 @@ $result = pg_query($conn, "select row_to_json(usuario_teste) from usuario_teste;
 
 if(pg_num_rows($result)){
     $resultArray = pg_fetch_all($result);
-    echo json_encode($resultArray);
+    echo json_encode($resultArray, JSON_PRETTY_PRINT);
     
 }
 //	echo (pg_fetch_row($result)[0]);
