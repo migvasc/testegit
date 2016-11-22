963be056434c0acd867fca0a7cf770f1fe3828f7
migvasc <miguel.vasconcelos@usp.br>
2016-11-22 00:05:02 +0000

    voltando a tentar colocar o mapa na home 5

--- home/index.php
+++ home/index.php
@@ -412,7 +412,7 @@
   <!-- container section start -->
 
     <!-- javascripts -->
-    <script src="../assets/js/mapa.js"></script>
+    
     <script src="js/jquery.js"></script>
 	<script src="js/jquery-ui-1.10.4.min.js"></script>
     <script src="js/jquery-1.8.3.min.js"></script>
@@ -452,6 +452,7 @@
 	<script src="js/sparklines.js"></script>	
 	<script src="js/charts.js"></script>
 	<script src="js/jquery.slimscroll.min.js"></script>
+	<script src="../assets/js/mapa.js"></script>
   <!--<script>
 
       //knob
